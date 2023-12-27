37cc64b70c0e1155a685772642aa3fb807b73766
Sinh vien(pass:123456) <phanquangminhhlqn@gmail.com>
2023-12-27 11:24:05 +0700

    Riel

---
+++ MyApplication/app/src/main/java/com/example/myapplication/tuan2/Tuan21TinhTongSecond.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class Tuan21TinhTongSecond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuan21_tinh_tong_second);
+    }
+}
